16281cd9a49789c70f6401e70c25ae59c919c94d
Cinthia M. Orona <devrider2021@gmail.com>
2023-04-02 17:58:59 -0300

    agregué boton normal.

--- my_custom/src/pages/index.js
+++ my_custom/src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Button from './components/button'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +16,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         Hello World
+      <Button></Button>
       </main>
     </>
   )
